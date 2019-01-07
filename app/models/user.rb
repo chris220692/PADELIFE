e1ed600cbad9e9a1cba5a26a908a7375d0a78255
Maxime Wetz <maxime-wetz@hotmail.com>
2019-01-07 17:32:17 +0100

    Add category to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :category, presence: true, inclusion: { in: ["Player", "Club", "Teacher"] }
 end
