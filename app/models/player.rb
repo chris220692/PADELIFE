b05d6acc599243d14f55dd912476faba5fa29b57
Maxime Wetz <maxime-wetz@hotmail.com>
2019-01-08 11:45:03 +0100

    photo uploaded on players

--- app/models/player.rb
+++ app/models/player.rb
@@ -6,7 +6,7 @@ class Player < ApplicationRecord
   validates :last_name, presence: true
   validates :age, presence: true, inclusion: { in: (12..99) }
   validates :level, presence: true, inclusion: { in: ["Beginner", "Elementary ", "Intermediate", "Advanced", "Expert"] }
-  #mount_uploader :photo, PhotoUploader
+  mount_uploader :photo, PhotoUploader
   validates :city, presence: true
   validates :zip, presence: true
   validates :gender, presence: true, inclusion: { in: ["Male", "Female"] }
